111301c26df57da33ba94d746e60f935d1da20fa
Wilson <w54545254121@gmail.com>
2024-01-08 22:03:06 +0800

    new group-user variables

--- create-attach-policies.tf
+++ create-attach-policies.tf
@@ -1,52 +1,52 @@
-# Define the list of regions
-variable "regions" {
-  default = [
-    "us-east-1",
-    "us-east-2",
-    "us-west-1",
-    "us-west-2",
-    "us-west-3"
-  ]
-}
+# # Define the list of regions
+# variable "regions" {
+#   default = [
+#     "us-east-1",
+#     "us-east-2",
+#     "us-west-1",
+#     "us-west-2",
+#     "us-west-3"
+#   ]
+# }
 
-# Create IAM policies for each region
-resource "aws_iam_policy" "region_policies" {
-  count = length(var.regions)
+# # Create IAM policies for each region
+# resource "aws_iam_policy" "region_policies" {
+#   count = length(var.regions)
 
-  name        = "policy-restrict-in-${var.regions[count.index]}"
-  description = "Restrict access to resources within ${var.regions[count.index]}"
+#   name        = "policy-restrict-in-${var.regions[count.index]}"
+#   description = "Restrict access to resources within ${var.regions[count.index]}"
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "aws:RequestedRegion": "$var.regions[count.index]"
-                }
-            }
-        }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": "*",
+#             "Resource": "*",
+#             "Condition": {
+#                 "StringEquals": {
+#                     "aws:RequestedRegion": "$var.regions[count.index]"
+#                 }
+#             }
+#         }
+#     ]
+#   })
+# }
 
-# Attach policies to IAM groups based on regions
-resource "aws_iam_policy_attachment" "attach_region_policies" {
-  count = length(var.regions)
+# # Attach policies to IAM groups based on regions
+# resource "aws_iam_policy_attachment" "attach_region_policies" {
+#   count = length(var.regions)
 
-  name       = aws_iam_policy.region_policies[count.index].name
-  policy_arn = aws_iam_policy.region_policies[count.index].arn
+#   name       = aws_iam_policy.region_policies[count.index].name
+#   policy_arn = aws_iam_policy.region_policies[count.index].arn
 
-  # Attach the policy to the respective IAM group based on the region
-  groups = [
-    aws_iam_group.new_iam_groups[count.index].name
-  ]
+#   # Attach the policy to the respective IAM group based on the region
+#   groups = [
+#     aws_iam_group.new_iam_groups[count.index].name
+#   ]
 
-  depends_on = [
-    aws_iam_group.new_iam_groups,
-    aws_iam_policy.region_policies
-  ]
-}
+#   depends_on = [
+#     aws_iam_group.new_iam_groups,
+#     aws_iam_policy.region_policies
+#   ]
+# }
--- create-users-and-groups.tf
+++ create-users-and-groups.tf
@@ -1,11 +1,24 @@
 # Create IAM users
-resource "aws_iam_user" "new_iam_users" {
-  count = length(var.fourty_test_emails)
-  name  = var.fourty_test_emails[count.index]  # Use email as username
+resource "aws_iam_user" "iam_users" {
+  count = length(flatten(values(var.groups_users)))
+  name = flatten(values(var.groups_users))[count.index]
 }
 
 # Create IAM groups
-resource "aws_iam_group" "new_iam_groups" {
-  count = length(var.five_test_groups)
-  name  = var.five_test_groups[count.index]
+resource "aws_iam_group" "iam_groups" {
+  count = length(keys(var.groups_users))
+  name = keys(var.groups_users)[count.index]
+}
+
+# Add users to groups
+resource "aws_iam_group_membership" "group_memberships" {
+  count = length(keys(var.groups_users))
+  name  = "group-membership-${aws_iam_group.iam_groups[count.index].name}-${count.index}"
+  group = aws_iam_group.iam_groups[count.index].name
+  users = var.groups_users[keys(var.groups_users)[count.index]]
+
+  depends_on = [
+    aws_iam_user.iam_users,
+    aws_iam_group.iam_groups
+   ]
 }
